2a9d418644474fb0c2eccc5a7300b86bc9dc44e8
johannpereze <sebas.jsp@gmail.com>
2021-07-30 13:58:50 -0500

    Agregué los siguientes ejercicios aun sin resolver

--- exercise1.js
+++ exercise1.js
@@ -360,7 +360,7 @@ const howManyYearsFrom = (date) => {
 //18)
 
 const countVowelsAndConsonants =(string)=>{
-  const vowels = string.match(/[aeiou]/ig)
+  const vowels = string.match(/[aeiouáéíóúü]/ig)
   const consonants = string.match(/[qwertypsdfghjklñzxcvbnm]/ig)
   console.log(`The sentence has ${vowels.length} vowels and ${consonants.length} consonants`);
   
@@ -407,3 +407,7 @@ const isValidEMail =(eMail = undefined)=>{
 // isValidEMail("sebas.jsp @gmail.com")
 // isValidEMail(654564564)
 // isValidEMail()
+
+// 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
+// 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+// 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
